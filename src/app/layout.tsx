9f11f4771ed99a1377c771d23693cbafe8fd6e82
Bennet-Ukoh <bennetukoh@gmail.com>
2024-08-08 15:50:17 +0100

    home page added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} text-sm text-zinc-900 bg-[#E5EBEC]`}>{children}</body>
+      <body
+        className={`${inter.className} text-sm text-zinc-900 bg-[#E5EBEC] min-h-screen`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
