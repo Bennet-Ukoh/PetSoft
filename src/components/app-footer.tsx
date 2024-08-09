210f7a45e089f847cb7861660afe8e4b5dacaa89
Bennet-Ukoh <bennetukoh@gmail.com>
2024-08-09 07:05:14 +0100

    footer added

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,3 +1,9 @@
 export default function AppFooter() {
-  return <div>App Footer</div>;
+  return (
+    <footer className="mt-auto py-5 border-t border-black/5">
+      <small className="opacity-50">
+        &copy; 2024 Bennet. All rights reserved.
+      </small>
+    </footer>
+  );
 }
