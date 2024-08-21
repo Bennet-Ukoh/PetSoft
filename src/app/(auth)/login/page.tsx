dce7c755684cfdd93e5f61ff1c69f49e27511906
Bennet-Ukoh <bennetukoh@gmail.com>
2024-08-21 17:41:01 +0100

    login and sign up ui added

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import AuthForm from "@/components/auth-form";
+import H1 from "@/components/h1";
+import Link from "next/link";
 
-export default function Login() {
-  return <div>Login Page</div>;
+export default function Page() {
+  return (
+    <main>
+      <H1 className="text-center">Log In</H1>
+      <AuthForm />
+      <p className="mt-2  text-sm">
+        No account yet?{" "}
+        <Link className="text-zinc-500 font-medium" href="/signup">
+          Sign Up
+        </Link>
+      </p>
+    </main>
+  );
 }
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,3 +1,18 @@
+import AuthForm from "@/components/auth-form";
+import H1 from "@/components/h1";
+import Link from "next/link";
+
 export default function Page() {
-  return <div>Sign Up page</div>;
+  return (
+    <main>
+      <H1 className="text-center">Sign Up</H1>
+      <AuthForm />
+      <p className="mt-2  text-sm">
+        Already have an account?{" "}
+        <Link className="text-zinc-500 font-medium" href="/signup">
+          Log In
+        </Link>
+      </p>
+    </main>
+  );
 }
